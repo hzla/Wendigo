2918964eb2c99f87173edb3e8c9a338947c2a9e1
hzla <neohzla@gmail.com>
2023-04-14 20:17:02 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/json'
+require 'sinatra/activerecord'
+require 'pry'
+
+Dir["models/*.rb"].each {|file| require_relative file}
+
+configure :development do 
+  set :database, {adapter: "postgresql", database: "wendigo"}
+end
+
+
+get '/' do 
+  binding.pry
+end
+
